a14c8d51f8f918e7479e3f82e738cfa1583692d3
Denys Nykyforov <popalay1@gmail.com>
2020-07-02 23:09:46 +0800

    Refactor Create tracker widget

--- app/src/main/java/com/popalay/tracktor/ui/create/CreateTrackedValue.kt
+++ app/src/main/java/com/popalay/tracktor/ui/create/CreateTrackedValue.kt
@@ -4,17 +4,21 @@ import androidx.compose.Composable
 import androidx.compose.state
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
+import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
-import androidx.ui.layout.ColumnScope.gravity
+import androidx.ui.foundation.shape.corner.CircleShape
 import androidx.ui.layout.InnerPadding
 import androidx.ui.layout.Row
 import androidx.ui.layout.Spacer
 import androidx.ui.layout.padding
+import androidx.ui.layout.preferredSize
 import androidx.ui.layout.width
 import androidx.ui.material.Button
 import androidx.ui.material.FilledTextField
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
+import androidx.ui.material.icons.Icons
+import androidx.ui.material.icons.filled.Add
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 
@@ -38,10 +42,11 @@ fun CreateTrackedValue(onSubmit: (String) -> Unit = {}) {
             )
             Spacer(modifier = Modifier.width(8.dp))
             Button(
-                text = { Text(text = "Submit") },
-                modifier = Modifier.gravity(Alignment.CenterHorizontally),
+                text = { Icon(asset = Icons.Default.Add) },
+                modifier = Modifier.gravity(Alignment.CenterVertically).preferredSize(48.dp),
                 backgroundColor = MaterialTheme.colors.secondary,
                 enabled = currentState.isNotBlank(),
+                shape = CircleShape,
                 onClick = {
                     onSubmit(currentState)
                     updateState("")
